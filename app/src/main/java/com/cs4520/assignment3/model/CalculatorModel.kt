1a96d580c514154cab8030fa8381ccdf253e9b1f
allychoung <allychoung@gmail.com>
2024-02-28 23:26:40 -0500

    update model

--- app/src/main/java/com/cs4520/assignment3/model/CalculatorModel.kt
+++ app/src/main/java/com/cs4520/assignment3/model/CalculatorModel.kt
@@ -5,7 +5,7 @@ import com.cs4520.assignment3.data.Operation
 class CalculatorModel {
     fun calculate(i1: Float, i2: Float, op: Operation): Number {
         if (i2.isNaN() || i1.isNaN()) {
-            throw IllegalArgumentException("Invalid user input. Try again.")
+            throw NumberFormatException()
         }
 
         when (op) {
@@ -13,7 +13,7 @@ class CalculatorModel {
             Operation.SUB -> return i1 - i2
             Operation.MUL -> return i1 * i2
             Operation.DIV -> if (i2.equals(0.0f)) {
-                throw IllegalArgumentException("Divisor cannot be 0. Try again.")
+                throw ArithmeticException("Divisor cannot be 0. Try again.")
             } else {
                 return i1 / i2
             }
